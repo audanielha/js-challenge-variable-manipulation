801cfe93963b170b9c04f4a11e47c8a7600c9a20
Daniel Ha <audanielha@gmail.com>
2024-08-05 22:47:21 +1200

    add variables and testing

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'Daniel';
+let age = 26;
+let isStudent = false;
+
+let greeting = `Hello my name is ${name}`;
+let nextYearAge = age + 1
+let statusMessage = isStudent ? "You are a student" : "You are not a student";
+
+console.log(greeting)
+console.log(nextYearAge)
+console.log(statusMessage)
